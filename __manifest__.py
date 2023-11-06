e9e01a4ebd2856102f060e3ac3da0c60caacdda7
Georgiy <iaseshviligeorgiy@gmail.com>
2023-11-06 13:36:16 +0200

    Empty module initialization

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Honestus',
+    'summary': 'Test module for honestus company',
+    'category': 'Website/Website',
+    'sequence': 20,
+    'version': '1.0',
+    'depends': [],
+    'installable': True,
+    'data': [],
+    'application': True,
+    'license': 'LGPL-3',
+}
