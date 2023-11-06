45bfc9fffd73fa7ad51be640a044d44ca6b8a8d8
Georgiy <iaseshviligeorgiy@gmail.com>
2023-11-07 01:33:32 +0200

    Task 4: sale.order.line was provided by neccessary fields

--- __manifest__.py
+++ __manifest__.py
@@ -7,6 +7,7 @@
     'depends': [
         'auth_signup',
         'portal',
+        'sale',
     ],
     'installable': True,
     'data': [
---
+++ models/sale_order_line.py
@@ -0,0 +1,19 @@
+from odoo import api, fields, models
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    code = fields.Char(string='Code', related='product_id.code')
+    honestus_code = fields.Char(string='Honestus Code', related='product_id.honestus_code')
+    honestus_price = fields.Float(string='Honestus Price', related='product_id.honestus_price')
+    margin = fields.Float(compute='_compute_margin', string='Margin')
+
+    @api.depends('price_unit', 'honestus_price', 'product_id.standard_price')
+    def _compute_margin(self):
+        for record in self:
+            base_price = record.product_id.honestus_price or record.price_unit
+            if base_price:
+                record.margin = (base_price - record.product_id.standard_price) / base_price
+            else:
+                record.margin = 0
