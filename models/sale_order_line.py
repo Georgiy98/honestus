a7640af2696b3000183814e73ac09c4fef8b2614
Georgiy <iaseshviligeorgiy@gmail.com>
2023-11-07 13:31:05 +0200

    Task 4: View for custom reports was added

--- models/sale_order_line.py
+++ models/sale_order_line.py
@@ -4,10 +4,10 @@ from odoo import api, fields, models
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    code = fields.Char(string='Code', related='product_id.code')
-    honestus_code = fields.Char(string='Honestus Code', related='product_id.honestus_code')
-    honestus_price = fields.Float(string='Honestus Price', related='product_id.honestus_price')
-    margin = fields.Float(compute='_compute_margin', string='Margin')
+    code = fields.Char(string='Code', related='product_id.code', store=True)
+    honestus_code = fields.Char(string='Honestus Code', related='product_id.honestus_code', store=True)
+    honestus_price = fields.Float(string='Honestus Price', related='product_id.honestus_price', store=True)
+    margin = fields.Float(compute='_compute_margin', string='Margin', store=True)
 
     @api.depends('price_unit', 'honestus_price', 'product_id.standard_price')
     def _compute_margin(self):
